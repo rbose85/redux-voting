a7bd6fd9ceac58c6a173e4cace43f0247e55f15b
Rajiv Bose <rbose85@users.noreply.github.com>
2015-12-28 17:23:53 +0100

    add simple example of immutable Number

---
+++ test/immutable_spec.js
@@ -0,0 +1,17 @@
+import {expect} from 'chai';
+
+describe('immutability', () => {
+    describe('a number', () => {
+        let increment = function (currentState) {
+            return currentState + 1;
+        };
+
+        it('is immutable', () => {
+            let state = 42;
+            let nextState = increment(state);
+
+            expect(nextState).to.equal(43);
+            expect(state).to.equal(42);
+        });
+    });
+});
